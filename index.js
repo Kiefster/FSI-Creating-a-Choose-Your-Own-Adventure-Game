2080584a151282a05e9e5db866e15bd0b6ddb2a4
Kiefer Vu <kiefervu@gmail.com>
2022-06-23 14:18:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Your Code here
+
 //Your Code here
 let left = 'You come across a stray cat. It scampers off down a small hole just large enough for you to crawl through. Do you follow it or continue on your path?';
 let follow = 'You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.';
